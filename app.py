2d3aefc388dc1090bd0ce1770cd968c5a8f8fd5a
baylagas <balbino_a@hotmail.com>
2021-07-19 00:01:19 -0600

    fixed card resources

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask
 from flask_restful import Api
-from resources.card_resource import Card
+from resources.card_verif_resource import CardVerify
+from resources.card_id_resource import CardId
+from resources.card_insert_resource import CardInsert
 
 
 app = Flask(__name__)
 api = Api(app)
 
 
-api.add_resource(Card, "/verify")
-#api.add_resource(Card, "/card/<int:number>")
-#api.add_resource(Card, "/insert")
+api.add_resource(CardVerify, "/verify")
+api.add_resource(CardId, "/card/<int:number>")
+api.add_resource(CardInsert, "/insert")
 
 if __name__ == "__main__":
     app.run(debug=True, port=12345)
